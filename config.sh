74097653421ebf92a5d9c6116ed236ca4ebb8c53
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2024-11-12 14:15:33 +0530

    adapt to PLE

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/nokia-msm8937-dev/twrp_device_nokia_PLE -b omni-9.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="PLE"
+export OEM="nokia"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
